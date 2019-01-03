217f78588cbdb45851daae8ad32f53d2d33401a8
pratyksha <32895655+pratyksha@users.noreply.github.com>
2019-01-03 15:58:44 +0530

    Python code to detect edges in a video

---
+++ edge_detect.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, img = cap.read()
+    laplacian = cv2.Laplacian(img, cv2.CV_64F)
+    edges = cv2.Canny(img, 100, 200)
+    print(edges)
+    cv2.imshow('edges', edges)
+    if cv2.waitKey(1)&0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()

f7f780855cb7c6719f46120a2dbc112927098397
pratyksha <32895655+pratyksha@users.noreply.github.com>
2019-04-04 23:07:18 +0530

    Update edge_detect.py

--- edge_detect.py
+++ edge_detect.py
@@ -1,3 +1,4 @@
+# import libraries
 import cv2
 import numpy as np
 
@@ -6,10 +7,10 @@ cap = cv2.VideoCapture(0)
 while True:
     ret, img = cap.read()
     laplacian = cv2.Laplacian(img, cv2.CV_64F)
-    edges = cv2.Canny(img, 100, 200)
+    edges = cv2.Canny(img, 100, 200) # apply canny edge detection 
     print(edges)
     cv2.imshow('edges', edges)
-    if cv2.waitKey(1)&0xFF == ord('q'):
+    if cv2.waitKey(1)&0xFF == ord('q'): # quit if q is pressed
         break
 cap.release()
 cv2.destroyAllWindows()
